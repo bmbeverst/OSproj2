e3d47a4d90ca0786dbdbdce82d5a5423760185e3
cyb3rkn19ht <bmb2gf@virginia.edu>
2011-03-11 02:53:55 -0500

    Implemented cd cwd and exit. Started on forking but ran it to errors.

--- mysh.c
+++ mysh.c
@@ -6,26 +6,73 @@
  */
 #include "mysh.h"
 
+int sizeArgs = 10;
+string args[10];// can't use sizeArgs because of error.
+pid_t pid;
+string binaryPath;
 
-char* promt = "mysh$ ";
-const char* envPromt = NULL;
-const char* home = NULL;
-
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
 	// initialize variables
+	string promt = "mysh$ ";
+	string envPromt = NULL;
+	string home = NULL;
 	envPromt = getenv("MYPS");
 	home = getenv("HOME");
+	binaryPath = "/bin/";
+
 	boolean run = 1;
 	int sizeInput = 256;
-	char* input = NULL;
+	string input = NULL;
+	int index;
 
+	if (envPromt != NULL) {
+		promt = envPromt;
+	}
 
-	while(run) {
+	//	pid = vfork();
+	//	if (pid == 0) {
+	//
+	//		fprintf(stdout, "FORKED");
+	//		//if (execvp(args[0], args) == -1) {
+	//			// Error logic
+	//		//}
+	//	} forks forever.
+	while (run) {
 
-		fprintf(stdout, "%s", input);
-		getline(&input, &sizeInput, stdin);
+		fprintf(stdout, "%s", promt);
+		getline(&input, (size_t *) &sizeInput, stdin);// please the compiler
+		args[0] = strtok(input, " \n");
 
+		for (index = 1; index < sizeArgs; index++) {
+			args[index] = strtok(NULL, " \n");
+		}
+		for (index = 0; index < 3; index++) {
+			fprintf(stdout, "%s\n", args[index]);
+		}
+		if (args[0] != NULL) {
+			if (strcmp(args[0], "exit") == 0) {
+				run = 0;
+			} else if(strcmp(args[0], "cd") == 0) {
+				if(args[1] != NULL) {
+					chdir(args[1]);
+				} else {
+					chdir(home);
+				}// used to test cd
+			} else if(strcmp(args[0], "cwd") == 0) {
+				getcwd(input, sizeInput);
+				fprintf(stdout, "%s\n", input);
+			}
+//			else {
+//				string temp = binaryPath;
+//				strcat(binaryPath, args[0]);
+//				FILE *filep = fopen(temp, "r");
+//				if(ferror(filep) || filep == NULL)
+//				{
+//					fprintf(stderr, "Cannot open file: %s\n", argv[1]);
+//					exit(1);
+//				}
+//			}
+		}
 
 	}
 
