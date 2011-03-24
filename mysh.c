3606864cfa649fd8617a5cfd653526102b500c2f
cyb3rkn19ht <bmb2gf@virginia.edu>
2011-03-24 19:54:57 -0400

    added malloc

--- mysh.c
+++ mysh.c
@@ -6,6 +6,8 @@
  */
 #include "mysh.h"
 
+
+int lengthArgs = 100;
 int sizeArgs = 10;
 string args[10];// can't use sizeArgs because of error.
 pid_t pid;
@@ -18,12 +20,15 @@ int main(int argc, char **argv) {
 	string home = NULL;
 	envPromt = getenv("MYPS");
 	home = getenv("HOME");
-	binaryPath = "/bin/";
 
 	boolean run = 1;
 	int sizeInput = 256;
-	string input = NULL;
 	int index;
+	string input = malloc(sizeof(char*)*sizeInput);
+	for(index = 0; index < sizeArgs; index++) {
+		args[index] = malloc(sizeof(char*)*sizeInput);
+	}
+
 
 	if (envPromt != NULL) {
 		promt = envPromt;
@@ -61,17 +66,18 @@ int main(int argc, char **argv) {
 			} else if(strcmp(args[0], "cwd") == 0) {
 				getcwd(input, sizeInput);
 				fprintf(stdout, "%s\n", input);
+			} else if(0) {
+
+			} else {
+				string temp = binaryPath;
+				strcat(binaryPath, args[0]);
+				FILE *filep = fopen(temp, "r");
+				if(ferror(filep) || filep == NULL)
+				{
+					fprintf(stderr, "Cannot open file: %s\n", argv[1]);
+					exit(1);
+				}
 			}
-//			else {
-//				string temp = binaryPath;
-//				strcat(binaryPath, args[0]);
-//				FILE *filep = fopen(temp, "r");
-//				if(ferror(filep) || filep == NULL)
-//				{
-//					fprintf(stderr, "Cannot open file: %s\n", argv[1]);
-//					exit(1);
-//				}
-//			}
 		}
 
 	}
